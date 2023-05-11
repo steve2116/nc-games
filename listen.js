bd211cf6909eb6d92590b9977a7377c07d300540
steve2116 <s.ackers@hotmail.co.uk>
2023-05-11 20:15:36 +0100

    Commit

--- listen.js
+++ listen.js
@@ -1,5 +1,8 @@
 const app = require("./app.js");
 
-app.listen(9090, (err) => {
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, (err) => {
     if (err) throw new Error("Error starting server");
+    else console.log(`Listening on port ${PORT}...`);
 });
