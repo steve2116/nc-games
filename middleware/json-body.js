93f40ba72e8089ee3227e00f0e71a90c2b28b095
steve2116 <s.ackers@hotmail.co.uk>
2023-05-15 10:51:00 +0100

    Fixed body format checking

--- middleware/json-body.js
+++ middleware/json-body.js
@@ -1,5 +1,5 @@
 module.exports = (request, response, next) => {
-    if (request.rawHeaders[5] !== "application/json")
+    if (!request.rawHeaders[5].includes("application/json"))
         next({ code: 400, msg: "Invalid body format" });
     else next();
 };
